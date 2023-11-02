b957bcc7198a068f7a1e799aedaf21d0319a59c9
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-02 14:55:03 +0100

    exercise 1.3 with specific exception catching

--- Exercise 1.3/src/Main.java
+++ Exercise 1.3/src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        divideByZero(4);
+        divideByZero(-3);
     }
 
     public static void divideByZero(int index) {
         int[] arr = {5, 7, 8, 9, 4, 7};
-        if (index < 0 || index >= arr.length) {
-            throw new RuntimeException("Index " + index + " out of bounds for length " + arr.length);
-        }
         try {
             var div = arr[index] / 0;
-        } catch (Exception e) {
-            throw new RuntimeException("Division by 0");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index " + index + " out of bound.");
+        } catch (ArithmeticException e) {
+            System.out.println("Division by 0");
         }
     }
 }
