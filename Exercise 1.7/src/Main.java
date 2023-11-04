429c4ae4f44953637f9a1fe1b5584cfbcdcce1ed
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-04 11:32:15 +0100

    exercise 1.7

---
+++ Exercise 1.7/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        var mySet = giveMeASet();
+        //String[] number = mySet.toArray(new String[mySet.size()]);
+
+        String toAdd = "new";
+        mySet.add(toAdd);
+        if(mySet.contains("new")){
+            System.out.println("the string " + toAdd + " has been added to the set");
+        }
+        System.out.println("Set:");
+        System.out.println(mySet);
+    }
+
+    public static HashSet<String> giveMeASet()
+    {
+        var mySet = new HashSet<String>();
+        Random r = new Random();
+        for(int i = 0; i < 10; ++i){
+            var n = r.nextInt(10) * 5;
+            mySet.add(String.valueOf(n));
+        }
+        return mySet;
+    }
+}
