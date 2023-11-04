4964c153e088917a17cb71152be9a7673ef4ffb6
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-04 09:51:18 +0100

    box unbox functions

--- Exercise 1.5/src/Main.java
+++ Exercise 1.5/src/Main.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        f1();
+        f2();
+        f3();
+        f4();
     }
 
-    public static void f()
+    public static void f1()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.print("number 1: ");
         int number1 = Integer.valueOf(scanner.nextLine().toString());
-        System.out.print("/nnumber 2: ");
+        System.out.print("number 2: ");
         int number2 = Integer.valueOf(scanner.nextLine().toString());
-
-        Integer autoboxedNumber1 = number1;
-        Integer autoboxedNumber2 = number2;
-
-        System.out.println("Sum of " + autoboxedNumber1 + " and " + autoboxedNumber2 + " is " + autoboxedNumber1 + autoboxedNumber2);
-
+        autoboxSum(number1,number2);
     }
 
     public static void f2()
@@ -27,29 +25,42 @@ public class Main {
         Integer number1 = Integer.valueOf(scanner.nextLine().toString());
         System.out.print("/nnumber 2: ");
         Integer number2 = Integer.valueOf(scanner.nextLine().toString());
-
-        int unboxedNumber1 = number1;
-        int unboxedNumber2 = number2;
-
-        System.out.println("Sum of " + unboxedNumber1 + " and " + unboxedNumber2 + " is " + unboxedNumber1 + unboxedNumber2);
-
+        unboxedSum(number1,number2);
     }
 
-    public static void m()
+    public static void autoboxSum(int a, int b)
+    {
+        System.out.println("[autobox]Sum of " + a + " and " + b + " is " + (a + b));
+    }
+
+    public static void unboxedSum(Integer a, Integer b)
+    {
+        System.out.println("[unbox]Sum of " + a + " and " + b + " is " + (a + b));
+    }
+
+    public static void f3()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a char: ");
+        char c = scanner.nextLine().toString().charAt(0);
+        autoboxPrintChar(c);
+    }
+
+    public static void f4()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a char: ");
         Character c = scanner.nextLine().toString().charAt(0);
-        char autoboxedc = c;
-        System.out.print("char: " + autoboxedc);
+        unboxPrintChar(c);
     }
 
-    public static void m2()
+    public static void autoboxPrintChar(Character c)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a char: ");
-        Character c = scanner.nextLine().toString().charAt(0);
-        char unboxedc = c;
-        System.out.print("char: " + unboxedc);
+        System.out.println("[autobox]char: " + c);
+    }
+
+    public static void unboxPrintChar(char c)
+    {
+        System.out.println("[unbox]char: " + c);
     }
 }
